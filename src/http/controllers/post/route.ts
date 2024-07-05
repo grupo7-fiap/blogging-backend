86c80646c0a73aabd8f9dd31e4f467ad32c0554c
pedrotuma <pedrotuma2000@gmail.com>
2024-07-05 10:41:39 -0300

    feat: Added routes to fetch all posts and a specific post by ID

--- src/http/controllers/post/route.ts
+++ src/http/controllers/post/route.ts
@@ -1,1 +1,9 @@
-// create express post route
+import { Router } from 'express';
+import { getPosts, getPostById } from './create';
+
+const router = Router();
+
+router.get('/', getPosts);
+router.get('/:id', getPostById);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import { env } from './lib/env';
 import { app } from './app';
+import { database } from './lib/mysql/db';
 
-app.listen(Number(env.PORT), '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${env.PORT}`);
+app.listen(Number(env.PORT), '0.0.0.0', async () => {
+  try {
+    const connection = await database.getConnection();
+    connection.release();
+    console.log('Connected to the database');
+    console.log(`Server is running on http://localhost:${env.PORT}`);
+  } catch (error) {
+    console.error('Failed to connect to the database', error);
+  }
 });
