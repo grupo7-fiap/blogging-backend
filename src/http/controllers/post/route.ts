af29ee6fcc36b3f72e5224c3e75037a402418940
Grupo 7 - Pós Tech FIAP <1fsdttechchallenge@gmail.com>
2024-07-23 20:11:38 -0300

    wip: create admin route

--- src/http/controllers/post/route.ts
+++ src/http/controllers/post/route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getPosts, getPostById, createPost } from './create';
+import { getAdminPosts } from './admin/get';
 
 const router = Router();
 
 router.get('/', getPosts);
 router.get('/:id', getPostById);
 router.post('/', createPost);
+router.get('/admin', getAdminPosts);
 
 export default router;
