ec4f24ad74ce1227b12e5f259f518ab29403d808
pedrotuma <pedrotuma2000@gmail.com>
2024-09-06 12:02:45 -0300

    inserting cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import postsRouter from './http/controllers/post/route';
-
+import cors from 'cors';
 export const app = express();
-
 // Configuração de middlewares
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3001' }));
+console.log('CORS middleware applied');
+
 
 // Configuração de rotas
 app.get('/', (req, res) => {
